39d8e01d1163997c9aa3da7a687f2bc55755408f
kuuuube <hexagonisalie@gmail.com>
2024-06-13 14:55:10 -0400

    Rename decode

--- src/main.rs
+++ src/main.rs
@@ -19,14 +19,14 @@ fn main() {
 }
 
 fn make_sudachi_dict() -> Result<JapaneseDictionary, Box<dyn std::error::Error>> {
-    let embedded_dictionary = decode(include_bytes!("./system_full.dic.zst"))?;
+    let embedded_dictionary = decode_zstd(include_bytes!("./system_full.dic.zst"))?;
     let dictionary_file_data = SudachiDicData::new(Storage::Owned(embedded_dictionary));
     let config = Config::new_embedded()?;
     let dictionary = JapaneseDictionary::from_embedded_storage(&config, dictionary_file_data)?;
     return Ok(dictionary);
 }
 
-pub fn decode(data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+pub fn decode_zstd(data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let bound = zstd_safe::decompress_bound(&data).expect("zstd_safe::decompress_bound failed");
     let mut decompressed: Vec<u8> = Vec::with_capacity(bound.try_into()?);
     zstd_safe::decompress(&mut decompressed, &data).expect("zstd_safe::decompress failed");
