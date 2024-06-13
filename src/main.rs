98b374be046e13fd5a54d6fdfd357ed6ca3f3aca
kuuuube <hexagonisalie@gmail.com>
2024-06-13 16:35:27 -0400

    Parse mokuro json file

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,23 @@ use sudachi::config::Config;
 use sudachi::dic::dictionary::JapaneseDictionary;
 use sudachi::dic::storage::{Storage, SudachiDicData};
 use sudachi::prelude::*;
+use serde::Deserialize;
 
 fn main() {
     let dict = make_sudachi_dict().unwrap();
     let tokenizer = StatelessTokenizer::new(&dict);
-    let line: String = "狐が大好き　何を見せてくれるんだ　わぁああっ！！".to_string();
+    let lines = get_json_file_data();
 
-    println!("{}", line);
-    let morphemes = tokenizer.tokenize(&line, Mode::C, false).unwrap();
-    for morpheme in morphemes.iter() {
-        println!("{:?}", morpheme);
+    let mut morpheme_surfaces: Vec<String> = Default::default();
+
+    for line in lines {
+        let morphemes = tokenizer.tokenize(&line, Mode::C, false).unwrap();
+        for morpheme in morphemes.iter() {
+            morpheme_surfaces.push(morpheme.surface().to_string());
+        }
     }
-    println!("Words found: {}", morphemes.len());
+    println!("{:?}", morpheme_surfaces);
+    println!("Words found: {}", morpheme_surfaces.len());
 }
 
 fn make_sudachi_dict() -> Result<JapaneseDictionary, Box<dyn std::error::Error>> {
@@ -26,9 +31,37 @@ fn make_sudachi_dict() -> Result<JapaneseDictionary, Box<dyn std::error::Error>>
     return Ok(dictionary);
 }
 
-pub fn decode_zstd(data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+fn decode_zstd(data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let bound = zstd_safe::decompress_bound(&data).expect("zstd_safe::decompress_bound failed");
     let mut decompressed: Vec<u8> = Vec::with_capacity(bound.try_into()?);
     zstd_safe::decompress(&mut decompressed, &data).expect("zstd_safe::decompress failed");
     return Ok(decompressed)
+}
+
+fn get_json_file_data() -> Vec<String> {
+    let json_data = std::fs::read_to_string("./mokuro_ocr_sample.json").unwrap();
+    let parsed_json: MokuroJson = serde_json::from_str(&json_data).unwrap();
+    let mut lines: Vec<String> = Default::default();
+    for block in parsed_json.blocks {
+        lines.push(block.lines.concat());
+    }
+    return lines;
+}
+
+#[derive(Debug, Deserialize)]
+struct MokuroJson {
+    //version: String,
+    //img_width: i32,
+    //img_height: i32,
+    blocks: Vec<MokuroBlock>
+}
+
+#[derive(Debug, Deserialize)]
+struct MokuroBlock {
+    //#[serde(rename = "box")]
+    //ocr_box: Vec<i32>,
+    //vertical: bool,
+    //font_size: i32,
+    //lines_coords: Vec<Vec<Vec<f32>>>,
+    lines: Vec<String>
 }
