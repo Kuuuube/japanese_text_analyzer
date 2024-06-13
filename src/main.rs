65c68986fd25be219c266c31be5ef340ee229645
kuuuube <hexagonisalie@gmail.com>
2024-06-13 14:32:39 -0400

    Compress dictionary file with zstd

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,17 @@ fn main() {
     println!("Words found: {}", morphemes.len());
 }
 
-fn make_sudachi_dict() -> Result<JapaneseDictionary, SudachiError> {
-    let embedded_dictionary = include_bytes!("./system_full.dic");
-    let dictionary_file_data = SudachiDicData::new(Storage::Borrowed(embedded_dictionary));
+fn make_sudachi_dict() -> Result<JapaneseDictionary, Box<dyn std::error::Error>> {
+    let embedded_dictionary = decode(include_bytes!("./system_full.dic.zst"))?;
+    let dictionary_file_data = SudachiDicData::new(Storage::Owned(embedded_dictionary));
     let config = Config::new_embedded()?;
     let dictionary = JapaneseDictionary::from_embedded_storage(&config, dictionary_file_data)?;
     return Ok(dictionary);
+}
+
+pub fn decode(data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let bound = zstd_safe::decompress_bound(&data).expect("zstd_safe::decompress_bound failed");
+    let mut decompressed: Vec<u8> = Vec::with_capacity(bound.try_into()?);
+    zstd_safe::decompress(&mut decompressed, &data).expect("zstd_safe::decompress failed");
+    return Ok(decompressed)
 }
