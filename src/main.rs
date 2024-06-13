f97c579e557bf0b99e3765639436abf1f128cf58
kuuuube <hexagonisalie@gmail.com>
2024-06-13 03:54:21 -0400

    Statically compile sudachi dict and config

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use std::path::PathBuf;
-
 use sudachi::analysis::stateless_tokenizer::StatelessTokenizer;
 use sudachi::analysis::Tokenize;
-use sudachi::config::Config;
+use sudachi::config::{Config, ConfigBuilder};
 use sudachi::dic::dictionary::JapaneseDictionary;
+use sudachi::dic::storage::{Storage, SudachiDicData};
 use sudachi::prelude::*;
 
 fn main() {
-    let config = Config::new(None, None, Some(PathBuf::from("./system_full.dic",))).unwrap(); //http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict/
-    let dict = JapaneseDictionary::from_cfg(&config).unwrap();
+    let dict = make_sudachi_dict();
     let tokenizer = StatelessTokenizer::new(&dict);
     let line: String = "狐が大好き　何を見せてくれるんだ　わぁああっ！！".to_string();
 
@@ -18,4 +16,12 @@ fn main() {
         println!("{:?}", morpheme);
     }
     println!("Words found: {}", morphemes.len());
+}
+
+fn make_sudachi_dict() -> JapaneseDictionary {
+    let embedded_dictionary = include_bytes!("./system_full.dic");
+    let dictionary_file_data = SudachiDicData::new(Storage::Borrowed(embedded_dictionary));
+    let config = Config::new_embedded().unwrap();
+    let dictionary = JapaneseDictionary::from_embedded_storage(&config, dictionary_file_data).unwrap();
+    return dictionary;
 }
