54c4e792506f4a1cfbe26efa850293593cec6693
kuuuube <hexagonisalie@gmail.com>
2024-06-15 13:05:59 -0400

    Add sorted occurrence list

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 
-pub fn analyze_morphemes(morpheme_surfaces: &Vec<String>) {
+pub fn generate_occurrence_list(morpheme_surfaces: &Vec<String>) -> Vec<(String, i32)> {
     let counts: HashMap<String, i32> = morpheme_surfaces.into_iter().fold(HashMap::new(), |mut map, c| {*map.entry(c.to_string()).or_insert(0) += 1; map});
+    let mut counts_sorted: Vec<(String, i32)> = counts.iter().map(|x| (x.0.to_owned(), x.1.to_owned())).collect();
+    counts_sorted.sort_by(|a, b| b.1.cmp(&a.1));
 
-    println!("{:?}", counts);
+    return counts_sorted;
 }
