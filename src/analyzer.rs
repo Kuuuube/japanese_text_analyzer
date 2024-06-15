a0205330c51c174c4a219952d64c11abd77ffa5e
kuuuube <hexagonisalie@gmail.com>
2024-06-15 13:30:12 -0400

    Make variable names better

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
 pub fn generate_occurrence_list(morpheme_surfaces: &Vec<String>) -> Vec<(String, i32)> {
-    let counts: HashMap<String, i32> = morpheme_surfaces.into_iter().fold(HashMap::new(), |mut map, c| {*map.entry(c.to_string()).or_insert(0) += 1; map});
-    let mut counts_sorted: Vec<(String, i32)> = counts.iter().map(|x| (x.0.to_owned(), x.1.to_owned())).collect();
-    counts_sorted.sort_by(|a, b| b.1.cmp(&a.1));
+    let morpheme_counts: HashMap<String, i32> = morpheme_surfaces.into_iter().fold(HashMap::new(), |mut map, c| {*map.entry(c.to_string()).or_insert(0) += 1; map});
+    let mut morpheme_counts_sorted: Vec<(String, i32)> = morpheme_counts.iter().map(|x| (x.0.to_owned(), x.1.to_owned())).collect();
+    morpheme_counts_sorted.sort_by(|a, b| b.1.cmp(&a.1));
 
-    return counts_sorted;
+    return morpheme_counts_sorted;
 }
