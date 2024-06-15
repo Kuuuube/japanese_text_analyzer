a9e829b50d2c9b584a8e4bd2db668f82a9911882
kuuuube <hexagonisalie@gmail.com>
2024-06-15 19:21:11 -0400

    Change wget command to powershell command in setup.bat

--- setup.bat
+++ setup.bat
@@ -1,2 +1,2 @@
-wget https://github.com/Kuuuube/sudachi_dicts/releases/latest/download/system_full.dic.zst
+powershell Invoke-WebRequest https://github.com/Kuuuube/sudachi_dicts/releases/latest/download/system_full.dic.zst -OutFile system_full.dic.zst
 move /y system_full.dic.zst src/system_full.dic.zst
