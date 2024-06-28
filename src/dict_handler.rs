11ae1e621256cb3599657fb9d29e0aa032707386
kuuuube <hexagonisalie@gmail.com>
2024-06-27 23:02:42 -0400

    Switch to main sudachi rs repo

--- src/dict_handler.rs
+++ src/dict_handler.rs
@@ -4,7 +4,7 @@ pub fn make_sudachi_dict() -> Result<JapaneseDictionary, Box<dyn std::error::Err
     let embedded_dictionary = decode_zstd(include_bytes!("./system_full.dic.zst"))?;
     let dictionary_file_data = SudachiDicData::new(Storage::Owned(embedded_dictionary));
     let config = sudachi::config::Config::new_embedded()?;
-    let dictionary = JapaneseDictionary::from_embedded_storage(&config, dictionary_file_data)?;
+    let dictionary = JapaneseDictionary::from_cfg_storage_with_embedded_chardef(&config, dictionary_file_data)?;
     return Ok(dictionary);
 }
 
