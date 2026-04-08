099e3a2c1ba6ae7b6b820eb05638e93e9721698b
kuuuube <hexagonisalie@gmail.com>
2026-04-07 22:02:11 -0400

    Split out stats handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::{
-    collections::{HashMap, HashSet},
     io::Read,
     sync::{Arc, RwLock},
 };
@@ -16,6 +15,7 @@ mod dict_handler;
 mod file_handler;
 mod tests;
 mod utf8_bufreader;
+mod stats_handler;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -53,7 +53,7 @@ fn main() {
 
     println!("Processing files, running tokenizer, and analyzing results");
     let start_time = std::time::Instant::now();
-    let stats = Arc::new(RwLock::new(AnalysisStats::default()));
+    let stats = Arc::new(RwLock::new(stats_handler::AnalysisStats::default()));
     let word_list_raw_file = Arc::new(RwLock::new(
         std::fs::File::create(&"word_list_raw.csv").expect("Failed to create word list raw file"),
     ));
@@ -62,7 +62,7 @@ fn main() {
             AnalysisType::MokuroJson => {
                 let lines = file_handler::get_json_file_data(file_path);
                 let morpheme_surfaces = run_tokenization(&lines, &tokenizer);
-                let new_stats = get_stats(lines, morpheme_surfaces, file_count, dir_count);
+                let new_stats = stats_handler::get_stats(lines, morpheme_surfaces, file_count, dir_count);
                 let word_list_raw_file_lock = &mut word_list_raw_file
                     .write()
                     .expect("Failed to get word_list_raw writer");
@@ -79,7 +79,7 @@ fn main() {
             AnalysisType::Mokuro => {
                 let lines = file_handler::get_mokuro_file_data(file_path);
                 let morpheme_surfaces = run_tokenization(&lines, &tokenizer);
-                let new_stats = get_stats(lines, morpheme_surfaces, file_count, dir_count);
+                let new_stats = stats_handler::get_stats(lines, morpheme_surfaces, file_count, dir_count);
                 let word_list_raw_file_lock = &mut word_list_raw_file
                     .write()
                     .expect("Failed to get word_list_raw writer");
@@ -99,7 +99,7 @@ fn main() {
                 {
                     buffered_plain_line_reader.par_bridge().for_each(|lines| {
                         let morpheme_surfaces = run_tokenization(&lines, &tokenizer);
-                        let new_stats = get_stats(lines, morpheme_surfaces, file_count, dir_count);
+                        let new_stats = stats_handler::get_stats(lines, morpheme_surfaces, file_count, dir_count);
                         let word_list_raw_file_lock = &mut word_list_raw_file
                             .write()
                             .expect("Failed to get word_list_raw writer");
@@ -252,135 +252,4 @@ fn run_tokenization(
         }
     }
     return morpheme_surfaces;
-}
-
-fn get_stats(
-    lines: Vec<String>,
-    morpheme_surfaces: Vec<String>,
-    json_file_count: usize,
-    json_dir_count: usize,
-) -> AnalysisStats {
-    let characters = morpheme_surfaces.join("");
-    let filtered_morphemes = analyzer::filter_blacklisted(&morpheme_surfaces);
-
-    let word_occurrence_list = analyzer::generate_occurrence_list(&filtered_morphemes);
-
-    let japanese_characters = analyzer::filter_non_japanese(&characters.chars().collect());
-    let kanji_characters: Vec<char> = analyzer::filter_non_kanji(&characters.chars().collect());
-    let kanji_occurrence_list = analyzer::generate_occurrence_list(&kanji_characters);
-    let mut unique_kanji_characters: Vec<char> = kanji_characters.clone();
-    unique_kanji_characters.sort();
-    unique_kanji_characters.dedup();
-
-    let box_length = analyzer::get_avg_len(lines).unwrap_or_default();
-
-    return AnalysisStats {
-        char_count: japanese_characters.len() as u64,
-        kanji_count: kanji_characters.len() as u64,
-        unique_kanji: HashSet::from_iter(unique_kanji_characters.iter().cloned()),
-        word_count: filtered_morphemes.len() as u64,
-        unique_words: HashSet::from_iter(word_occurrence_list.iter().map(|x| x.0.to_owned())),
-        volume_count: json_dir_count,
-        avg_volume_length: japanese_characters.len() as f64 / json_dir_count as f64,
-        page_count: json_file_count,
-        avg_page_length: japanese_characters.len() as f64 / json_file_count as f64,
-        avg_box_length: box_length.average as f64,
-        shortest_box_length: box_length.shortest,
-        longest_box_length: box_length.longest,
-        box_count: box_length.length as u64,
-
-        word_list_raw: filtered_morphemes,
-        kanji_occurrence_list: kanji_occurrence_list,
-        word_occurrence_list: word_occurrence_list,
-    };
-}
-
-#[derive(Debug)]
-struct AnalysisStats {
-    char_count: u64,
-    kanji_count: u64,
-    unique_kanji: HashSet<char>,
-    word_count: u64,
-    unique_words: HashSet<String>,
-    volume_count: usize,
-    avg_volume_length: f64,
-    page_count: usize,
-    avg_page_length: f64,
-    avg_box_length: f64,
-    shortest_box_length: usize,
-    longest_box_length: usize,
-    box_count: u64,
-
-    word_list_raw: Vec<String>,
-    kanji_occurrence_list: HashMap<char, u64>,
-    word_occurrence_list: HashMap<String, u64>,
-}
-
-impl Default for AnalysisStats {
-    fn default() -> Self {
-        Self {
-            char_count: Default::default(),
-            kanji_count: Default::default(),
-            unique_kanji: Default::default(),
-            word_count: Default::default(),
-            unique_words: Default::default(),
-            volume_count: Default::default(),
-            avg_volume_length: Default::default(),
-            page_count: Default::default(),
-            avg_page_length: Default::default(),
-            avg_box_length: Default::default(),
-            shortest_box_length: usize::MAX,
-            longest_box_length: Default::default(),
-            box_count: Default::default(),
-            word_list_raw: Default::default(),
-            kanji_occurrence_list: Default::default(),
-            word_occurrence_list: Default::default(),
-        }
-    }
-}
-
-impl AnalysisStats {
-    fn combine(&mut self, stats2: AnalysisStats) {
-        *self = AnalysisStats {
-            char_count: self.char_count + stats2.char_count,
-            kanji_count: self.kanji_count + stats2.kanji_count,
-            unique_kanji: self
-                .unique_kanji
-                .union(&stats2.unique_kanji)
-                .map(|x| x.to_owned())
-                .collect(),
-            word_count: self.word_count + stats2.word_count,
-            unique_words: self
-                .unique_words
-                .union(&stats2.unique_words)
-                .map(|x| x.to_owned())
-                .collect(),
-            volume_count: stats2.volume_count,
-            avg_volume_length: self.avg_volume_length + stats2.avg_volume_length,
-            page_count: stats2.page_count,
-            avg_page_length: self.avg_page_length + stats2.avg_page_length,
-            avg_box_length: f64::max(
-                (self.avg_box_length * self.box_count as f64
-                    + stats2.avg_box_length * stats2.box_count as f64)
-                    / (self.box_count + stats2.box_count) as f64,
-                0.0, // cure potential NaN corruption by overriding NaNs with 0.0
-            ),
-            shortest_box_length: analyzer::bounded_min(
-                self.shortest_box_length,
-                stats2.shortest_box_length,
-                1,
-            ),
-            longest_box_length: usize::max(self.longest_box_length, stats2.longest_box_length),
-            box_count: self.box_count + stats2.box_count,
-            word_list_raw: vec![],
-            kanji_occurrence_list: analyzer::merge_hashmap(
-                stats2.kanji_occurrence_list,
-                &self.kanji_occurrence_list,
-            ),
-            word_occurrence_list: analyzer::merge_hashmap(
-                stats2.word_occurrence_list,
-                &self.word_occurrence_list,
-            ),
-        };
-    }
 }
---
+++ src/stats_handler.rs
@@ -0,0 +1,134 @@
+use std::collections::{HashMap, HashSet};
+
+use crate::analyzer;
+
+pub fn get_stats(
+    lines: Vec<String>,
+    morpheme_surfaces: Vec<String>,
+    json_file_count: usize,
+    json_dir_count: usize,
+) -> AnalysisStats {
+    let characters = morpheme_surfaces.join("");
+    let filtered_morphemes = analyzer::filter_blacklisted(&morpheme_surfaces);
+
+    let word_occurrence_list = analyzer::generate_occurrence_list(&filtered_morphemes);
+
+    let japanese_characters = analyzer::filter_non_japanese(&characters.chars().collect());
+    let kanji_characters: Vec<char> = analyzer::filter_non_kanji(&characters.chars().collect());
+    let kanji_occurrence_list = analyzer::generate_occurrence_list(&kanji_characters);
+    let mut unique_kanji_characters: Vec<char> = kanji_characters.clone();
+    unique_kanji_characters.sort();
+    unique_kanji_characters.dedup();
+
+    let box_length = analyzer::get_avg_len(lines).unwrap_or_default();
+
+    return AnalysisStats {
+        char_count: japanese_characters.len() as u64,
+        kanji_count: kanji_characters.len() as u64,
+        unique_kanji: HashSet::from_iter(unique_kanji_characters.iter().cloned()),
+        word_count: filtered_morphemes.len() as u64,
+        unique_words: HashSet::from_iter(word_occurrence_list.iter().map(|x| x.0.to_owned())),
+        volume_count: json_dir_count,
+        avg_volume_length: japanese_characters.len() as f64 / json_dir_count as f64,
+        page_count: json_file_count,
+        avg_page_length: japanese_characters.len() as f64 / json_file_count as f64,
+        avg_box_length: box_length.average as f64,
+        shortest_box_length: box_length.shortest,
+        longest_box_length: box_length.longest,
+        box_count: box_length.length as u64,
+
+        word_list_raw: filtered_morphemes,
+        kanji_occurrence_list: kanji_occurrence_list,
+        word_occurrence_list: word_occurrence_list,
+    };
+}
+
+#[derive(Debug)]
+pub struct AnalysisStats {
+    pub char_count: u64,
+    pub kanji_count: u64,
+    pub unique_kanji: HashSet<char>,
+    pub word_count: u64,
+    pub unique_words: HashSet<String>,
+    pub volume_count: usize,
+    pub avg_volume_length: f64,
+    pub page_count: usize,
+    pub avg_page_length: f64,
+    pub avg_box_length: f64,
+    pub shortest_box_length: usize,
+    pub longest_box_length: usize,
+    pub box_count: u64,
+
+    pub word_list_raw: Vec<String>,
+    pub kanji_occurrence_list: HashMap<char, u64>,
+    pub word_occurrence_list: HashMap<String, u64>,
+}
+
+impl Default for AnalysisStats {
+    fn default() -> Self {
+        Self {
+            char_count: Default::default(),
+            kanji_count: Default::default(),
+            unique_kanji: Default::default(),
+            word_count: Default::default(),
+            unique_words: Default::default(),
+            volume_count: Default::default(),
+            avg_volume_length: Default::default(),
+            page_count: Default::default(),
+            avg_page_length: Default::default(),
+            avg_box_length: Default::default(),
+            shortest_box_length: usize::MAX,
+            longest_box_length: Default::default(),
+            box_count: Default::default(),
+            word_list_raw: Default::default(),
+            kanji_occurrence_list: Default::default(),
+            word_occurrence_list: Default::default(),
+        }
+    }
+}
+
+impl AnalysisStats {
+    pub fn combine(&mut self, stats2: AnalysisStats) {
+        *self = AnalysisStats {
+            char_count: self.char_count + stats2.char_count,
+            kanji_count: self.kanji_count + stats2.kanji_count,
+            unique_kanji: self
+                .unique_kanji
+                .union(&stats2.unique_kanji)
+                .map(|x| x.to_owned())
+                .collect(),
+            word_count: self.word_count + stats2.word_count,
+            unique_words: self
+                .unique_words
+                .union(&stats2.unique_words)
+                .map(|x| x.to_owned())
+                .collect(),
+            volume_count: stats2.volume_count,
+            avg_volume_length: self.avg_volume_length + stats2.avg_volume_length,
+            page_count: stats2.page_count,
+            avg_page_length: self.avg_page_length + stats2.avg_page_length,
+            avg_box_length: f64::max(
+                (self.avg_box_length * self.box_count as f64
+                    + stats2.avg_box_length * stats2.box_count as f64)
+                    / (self.box_count + stats2.box_count) as f64,
+                0.0, // cure potential NaN corruption by overriding NaNs with 0.0
+            ),
+            shortest_box_length: analyzer::bounded_min(
+                self.shortest_box_length,
+                stats2.shortest_box_length,
+                1,
+            ),
+            longest_box_length: usize::max(self.longest_box_length, stats2.longest_box_length),
+            box_count: self.box_count + stats2.box_count,
+            word_list_raw: vec![],
+            kanji_occurrence_list: analyzer::merge_hashmap(
+                stats2.kanji_occurrence_list,
+                &self.kanji_occurrence_list,
+            ),
+            word_occurrence_list: analyzer::merge_hashmap(
+                stats2.word_occurrence_list,
+                &self.word_occurrence_list,
+            ),
+        };
+    }
+}
